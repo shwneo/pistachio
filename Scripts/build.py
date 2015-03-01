31d035c9b5362453419a06619074e254477d914d
hpxue <hpxue@outlook.com>
2015-03-01 15:19:58 +0800

    Separate and abstract the common part of the building py scripts
    All building parts derived from build.BaseBuild class

---
+++ Scripts/build.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import os
+import platform
+import ConfigParser
+
+class BaseBuild:
+
+	def __init__(self, name=None):
+		self.__conf =  ConfigParser.ConfigParser()
+		self.__conf.read('3rd_party.conf')
+		self.__name = name
+		self.line_end = '\n'
+		if platform.system() == 'Windows':
+			self.line_end = '\r\n'
+		self.pwd = os.getcwd()
+
+	def install_dist(self):
+		if self.__name is None:
+			raise Exception("BaseBuild: Cannot determine the building target, give up")
+		return self.pwd + os.sep + self.__conf.get(self.__name, 'dir') + os.sep
+
+	def source_name(self):
+		if self.__name is None:
+			raise Exception("BaseBuild: Cannot determine the building target, give up")
+		return self.__conf.get(self.__name, 'dir')
+
+	def check_srouce(self):
+		if self.__name is None:
+			raise Exception("BaseBuild: Cannot determine the building target, give up")
+		tools_files = os.listdir('../build')
+		for file in tools_files:
+			if file == self.__conf.get(self.__name, 'dir'):
+				return True
+		return False
+
+	def check_pack(self):
+		if self.__name is None:
+			raise Exception("BaseBuild: Cannot determine the building target, give up")
+		tools_files = os.listdir('../build')
+		for file in tools_files:
+			if file == self.__conf.get(self.__name, 'pack'):
+				return True
+		return False
+
+	def download_source(self):
+		if self.__name is None:
+			raise Exception("BaseBuild: Cannot determine the building target, give up")
+		os.system('cd ../build && python ../Tools/bin/pwget.py %s' % (self.__conf.get(self.__name, 'source')))
+
+	def unpack_source(self):
+		if self.__name is None:
+			raise Exception("BaseBuild: Cannot determine the building target, give up")
+		os.system('cd ../build && python ../Tools/bin/ptar.py %s %s' % (self.__conf.get(self.__name, 'unpack'),
+																		self.__conf.get(self.__name, 'pack')))
+
--- Scripts/build_perl.py
+++ Scripts/build_perl.py
@@ -1,49 +1,23 @@
 #!/usr/bin/python
+from build import BaseBuild
 import os
 import platform
-import ConfigParser
 
-class PerlBuilding:
+class PerlBuilding(BaseBuild):
 
 	def __init__(self):
-		self.__conf =  ConfigParser.ConfigParser()
-		self.__conf.read('3rd_party.conf')
-		self.__line_end = '\n'
-		self.__pwd = os.getcwd()
-
-
-	def check_perl_srouce(self):
-		tools_files = os.listdir('../build')
-		for file in tools_files:
-			print(file)
-			if file == self.__conf.get('perl', 'dir'):
-				return True
-		return False
-
-	def check_perl_pack(self):
-		tools_files = os.listdir('../build')
-		for file in tools_files:
-			if file == self.__conf.get('perl', 'pack'):
-				return True
-		return False
-
-	def download_source(self):
-		os.system('cd ../build && python ../Tools/bin/pwget.py %s' % (self.__conf.get('perl', 'source')))
-
-	def unpack_source(self):
-		os.system('cd ../build && python ../Tools/bin/ptar.py %s %s' % (self.__conf.get('perl', 'unpack'),
-																		self.__conf.get('perl', 'pack')))
+		BaseBuild.__init__(self, 'perl')
 
 	def fix_make_file(self):
-		mk_file_name = '../build/%s/win32/Makefile' % self.__conf.get('perl', 'dir')
+		mk_file_name = '../build/%s/win32/Makefile' % self.source_name()
 		if (os.path.isfile(mk_file_name)):
 			with open(mk_file_name, 'r+') as mk_file:
 				mk_lines = mk_file.readlines()
 				line_num = 0
 				for line in mk_lines:
 					if line.startswith('INST_TOP'):
-						print(line)
-						mk_lines[line_num] = 'INST_TOP\t= ' + self.__pwd + os.sep + self.__conf.get('perl', 'dist') + os.sep
+						mk_lines[line_num] = 'INST_TOP\t= ' + self.install_dist() + self.line_end
+						print('New INST_TOP:\n' + line)
 					line_num = line_num + 1
 					
 			with open(mk_file_name, 'w+') as mk_file:
@@ -52,15 +26,21 @@ class PerlBuilding:
 			print('No valid Makefile found, please clean the build');
 			exit(1)
 
+	def do_build_win32(self):
+		self.fix_make_file()
+		os.system("cd ../build && build_perl.bat %s" % self.source_name())
+
 
 def main():
 	perl_building = PerlBuilding()
-	if not perl_building.check_perl_srouce():
-		if not perl_building.check_perl_pack():
+	if not perl_building.check_srouce():
+		if not perl_building.check_pack():
 			perl_building.download_source()
 		perl_building.unpack_source()
-
-	perl_building.fix_make_file()
+	if platform.system() == 'Windows':
+		perl_building.do_build_win32()
+	else:
+		pass
 
 if __name__ == '__main__':
 	exit(main())
