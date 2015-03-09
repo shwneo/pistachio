9eafa1b26520233cd18853bf9fcd537606671b37
shwneo <shwneo@hotmail.com>
2015-03-09 16:12:04 +0800

    python lexer finished!

--- Scripts/python_lexer.py
+++ Scripts/python_lexer.py
@@ -1,30 +1,33 @@
 import ply.lex as lex
 
-tokens = (
-	'CLASS',
-	'DEF',
-	'IF',
-	'ELSE',
-	'ELIF',
-	'FOR',
-	'IN',
-	'WHILE',
-	'BREAK',
-	'EXCEPT',
-	'RAISE',
-	'IMPORT',
-	'FROM',
-	'AS',
-	'WITH',
-	'IS',
-	'RETURN',
-	'CONTINUE',
-	'LAMBDA',
-	'TRY',
-	'AND',
-	'OR',
-	'NOT',
-	'ASSERT',
+key_words = {
+	'class':'CLASS',
+	'def':'DEF',
+	'if':'IF',
+	'else':'ELSE',
+	'elif':'ELIF',
+	'for':'FOR',
+	'in':'IN',
+	'while':'WHILE',
+	'break':'BREAK',
+	'except':'EXCEPT',
+	'raise':'RAISE',
+	'import':'IMPORT',
+	'from':'FROM',
+	'as':'AS',
+	'with':'WITH',
+	'is':'IS',
+	'return':'RETURN',
+	'continue':'CONTINUE',
+	'lambda':'LAMBDA',
+	'try':'TRY',
+	'and':'AND',
+	'or':'OR',
+	'not':'NOT',
+	'assert':'ASSERT',
+}
+
+tokens = [
 	'LPAREN',
   	'RPAREN',
   	'COMMA',
@@ -43,7 +46,10 @@ tokens = (
 	'TIMES',
 	'DIVIDE',
 	'INTEGER',
-	#'FLOAT',
+	'HEX',
+	'OCT',
+	'FLOAT',
+	'COMPLEX',
 	'COMMENT',
 	'SIGQUOT',
 	'DOUBLEQUOT',
@@ -52,104 +58,95 @@ tokens = (
 	'NEWLINE',
 	'PERCENT',
 	'COLON',
+	'BASESTRING',
+	'LONGSTRING',
+	'LESSTHAN',
+	'BIGGERTHAN',
+	'LESSEQUAL',
+	'BIGGEREQUAL',
+	'NOTEQUAL',
+	'BITOR',
+	'BITAND',
+	'BITXOR',
+	'LSHIFT',
+	'RSHIFT',
+	'MOD',
+	'COMPLEMENT',
+	'POWER',
 	'IDENTIFIER',
+] + list(key_words.values())
+
+states = (
+   ('basestring','exclusive'),
+   ('longstring', 'exclusive'),
 )
 
-def t_IMPORT(t):
-	r'import$'
+def t_IDENTIFIER(t):
+	r'[_a-zA-Z][\d\w]*'
+	t.type = key_words.get(t.value,'IDENTIFIER')
 	return t
 
-def t_CLASS(t):
-	r'class$'
+def t_OCT(t):
+	r'0[1-7]+[0-7]*[Ll]?'
 	return t
 
-def t_DEF(t):
-	r'def$'
+def t_COMPLEX(t):
+	r'\d+j[$\s]'
+	t.value = t.value.strip()
 	return t
 
-def t_IF(t):
-	r'if$'
+def start_string(t, long_str=False):
+	t.lexer.quot_mark = t.value
+	t.lexer.start_pos = t.lexer.lexpos
+	t.lexer.long_str = long_str
+	if not long_str:
+		t.lexer.push_state('basestring')
+	else:
+		t.lexer.push_state('longstring')
 	return t
 
-def t_ELSE(t):
-	r'else$'
-	return t
+def t_TRIPLEQUOT(t):
+	r'[\'\"]{3}'
+	return start_string(t, True)
 
-def t_ELIF(t):
-	r'elif$'
-	return t
+def t_DOUBLEQUOT(t):
+	r'\"'
+	return start_string(t)
 
-def t_FOR(t):
-	r'for$'
-	return t
+def t_SIGQUOT(t):
+	r'\''
+	return start_string(t)
 
-def t_IN(t):
-	r'in$'
-	return t
+def t_basestring_end(t):
+	r'[\'\"]'
+	if t.value == t.lexer.quot_mark:
+		t.value = t.lexer.lexdata[t.lexer.start_pos:t.lexer.lexpos]
+		t.type = 'BASESTRING'
+		t.lexer.pop_state()
+		return t
 
-def t_WHILE(t):
-	r'while$'
-	return t
+def t_longstring_end(t):
+	r'(\'|\"){3}'
+	if t.value == t.lexer.quot_mark:
+		t.value = t.lexer.lexdata[t.lexer.start_pos:t.lexer.lexpos]
+		t.type = 'LONGSTRING'
+		t.lexer.pop_state()
+		return t
 
-def t_BREAK(t):
-	r'break$'
-	return t
+def process_string(t):
+	if t.value[0] == '\\':
+		t.lexer.skip(2)
+		return
+	else:
+		t.lexer.skip(1)
+	if t.value[0] == '\n' and not t.lexer.long_str:
+		raise Exception("Unexpected end of line at %d" % t.lexer.lexpos)
 
-def t_EXCEPT(t):
-	r'except$'
-	return t
+def t_basestring_error(t):
+	process_string(t)
 
-def t_RAISE(t):
-	r'raise$'
-	return t
-
-def t_FROM(t):
-	r'from$'
-	return t
-
-def t_AS(t):
-	r'as$'
-	return t
-
-def t_WITH(t):
-	r'with$'
-	return t
-
-def t_IS(t):
-	r'is$'
-	return t
-
-def t_RETURN(t):
-	r'return$'
-	return t
-
-def t_CONTINUE(t):
-	r'continue$'
-	return t
-
-def t_LAMBDA(t):
-	r'lambda'
-	return t
-
-def t_TRY(t):
-	r'try$'
-	return t
-
-def t_AND(t):
-	r'and$'
-	return t
-
-def t_OR(t):
-	r'or$'
-	return t
-
-def t_NOT(t):
-	r'not$'
-	return t
-
-def t_ASSERT(t):
-	r'assert'
-	return
+def t_longstring_error(t):
+	process_string(t)
 
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
@@ -161,28 +158,41 @@ t_LBRACE = r'\{'
 t_RBRACE = r'\}'
 t_SLASH = r'\/'
 t_BACKSLASH = r'\\'
-t_INTEGER = r'\d+([eE]\d+)?'
-#t_FLOAT = r'[(\.\d+)(\d+\.\d+)](eE\d+)?'
+t_INTEGER = r'\d+([eE][+-]?\d+)?[Ll]?'
+t_HEX = r'0x[0-9a-fA-F]+[Ll]?$'
+t_FLOAT = r'([0-9]*\.[0-9]+|[0-9]+\.[0-9]*)([eE][+-]?[0-9]+)?'
 t_PLUS = r'\+'
 t_MINUS = r'\-'
 t_TIMES = r'\*'
 t_DIVIDE = r'\/'
 t_ignore_COMMENT = r'\#.*'
-t_SIGQUOT = r'\''
-t_DOUBLEQUOT = r'\"'
-t_TRIPLEQUOT = r'[\'\"]{3}'
-t_IDENTIFIER = r'[_a-zA-Z][\d\w]*'
+
 t_WHITESPACE = r'[ \t]+'
 t_NEWLINE = r'\n'
-t_ASSIGN = '='
-t_EQUAL = '=='
-t_PERCENT = '\%'
-t_COLON = ':'
+t_ASSIGN = r'='
+t_EQUAL = r'=='
+t_PERCENT = r'\%'
+t_COLON = r':'
+t_LESSTHAN = r'<'
+t_BIGGERTHAN = r'>'
+t_LESSEQUAL = r'<='
+t_BIGGEREQUAL = r'>='
+t_NOTEQUAL = r'!='
+t_BITOR = r'\|'
+t_BITAND = r'\&'
+t_BITXOR = r'\^'
+t_LSHIFT = r'<<'
+t_RSHIFT = r'>>'
+t_MOD = r'%'
+t_COMPLEMENT = r'~'
+t_POWER = r'\*\*'
 
 lexer = lex.lex()
 out_file = open('.\\lexer_out.txt', 'w')
-with open('C:\\Python27\\Lib\\lib2to3\\fixer_base.py','r') as input_file:
+with open('C:\\Python27\\Lib\\sha.py','r') as input_file:
+#with open('C:\\Python27\\Lib\\lib2to3\\test.py') as input_file:
 	input_text = input_file.read()
+	input_text.decode('utf-8')
 	lexer.input(input_text)
 	while True:
 		tok = lexer.token()
